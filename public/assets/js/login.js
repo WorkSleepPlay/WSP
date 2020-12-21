cbc26a412695c8c9a1bca93db1c7c31e412a3687
marandaz <mrzwies@gmail.com>
2020-12-20 19:11:58 -0600

    updates to login.js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   // Getting references to our form and inputs
-  var loginForm = $("form.login");
+  var loginForm = $("loginForm");
   var emailInput = $("input#email");
   var passwordInput = $("input#password");
   var ageInput = $("input#age");
@@ -12,6 +12,7 @@ $(document).ready(function () {
 
   // When the form is submitted, we validate there's an email and password entered
   loginForm.on("submit", function (event) {
+    console.log("submit")
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
@@ -21,6 +22,7 @@ $(document).ready(function () {
     if (!userData.email || !userData.password) {
       return;
     }
+    console.log("userData", userData)
 
     // If we have an email and password we run the loginUser function and clear the form
     loginUser(userData.email, userData.password);
@@ -30,26 +32,27 @@ $(document).ready(function () {
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the profile page
   function loginUser(email, password) {
+    console.log("log in user")
     $.post("/api/login", {
       email: email,
       password: password,
     })
       .then(function () {
-        $.get("/api/user_data/" + emailInput, function (data) {
-          console.log(data);
-          if (data) {
-            fullName = data.fullName;
-            emailEntry = data.email;
-            age = data.age;
-            id = data.id;
-          }
-        })
-
+        console.log("success login")
+        // $.get("/api/user_data/" + emailInput, function (data) {
+        //   console.log(data);
+        //   if (data) {
+        //     fullName = data.fullName;
+        //     emailEntry = data.email;
+        //     age = data.age;
+        //     id = data.id;
+        //   }
+        // })
         window.location.replace("/profile");
         // If there's an error, log the error
       })
       .catch(function (err) {
-        console.log(err);
+        console.log("log in error", err);
       });
   }
 });
