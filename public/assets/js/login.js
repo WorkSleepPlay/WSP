10d7abf82102e18ac5b1a08a422b3b76975ed7ea
Selma Elgab <69071329+selma-e@users.noreply.github.com>
2020-12-20 22:20:45 -0800

    Update login.js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   // Getting references to our form and inputs
-  var loginForm = $("form-login");
+  var loginForm = $("log-in-form");
   var emailInput = $("#email");
   var passwordInput = $("#userPassword");
   var fullName;
