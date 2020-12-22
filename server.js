634388c0134ec2b6c8110c441fd49d35b1680c56
marandaz <mrzwies@gmail.com>
2020-12-22 13:01:57 -0600

    removing comments

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-// Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
-// Requiring passport as we've configured it
 var exphbs = require("express-handlebars");
 var passport = require("./config/passport");
-
-// Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
-// Creating express app and configuring middleware needed for authentication
 var app = express();
 app.use(
   express.urlencoded({
@@ -27,7 +22,6 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-// Set Handlebars as the default templating engine.
 app.engine(
   "handlebars",
   exphbs({
@@ -35,12 +29,8 @@ app.engine(
   })
 );
 app.set("view engine", "handlebars");
-
-// Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
-
-// We need to use sessions to keep track of our user's login status
 app.use(
   session({
     secret: "keyboard cat",
