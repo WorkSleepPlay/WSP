39e80083d9c1ffb1959bf4091f8dac38178051de
Selma Elgabalawy <salmosha88@gmail.com>
2020-12-22 12:56:56 -0800

    adding profile javascript ajax get

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
   var id;
 
   profileInfo();
+  userData();
 
   function profileInfo() {
     $.get("/api/user", function (req, res) {
@@ -16,37 +17,14 @@ $(document).ready(function () {
       }
     });
   }
-  console.log(age);
-  console.log(email);
-  console.log(fullName);
-  console.log(id);
+
+  function userData() {
+    $.get("/api/userdata", function (req, res) {
+      if (req) {
+        work = req.body.work;
+        sleep = req.body.sleep;
+        play = req.body.play;
+      }
+    });
+  }
 });
-
-//   delete button ID is "delete-button"
-
-// Retrieve the template data from the HTML (jQuery is used here).
-// var template = $("/profile.handlebars").html();
-
-// // Compile the template data into a function
-// var templateScript = Handlebars.compile(template);
-
-// var newID = {
-//   fullMame: "fullName",
-//   email: "email",
-//   age: "age",
-// };
-// Send the POST request.
-// $.ajax("/api/profile", {
-//   type: "POST",
-//   data: newID
-// }).then(
-//   function() {
-//     console.log("user info");
-// Reload the page to get the updated list
-//     location.reload();
-//   }
-// );
-// var html = templateScript(context);
-
-// Insert the HTML code into the page
-// $(document.body).append(html);
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,58 +3,27 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
   //SIGNUP HTML ROUTES
-  // app.get("/signup", function (req, res) {
-  //   // if (req.user) {
-  //   //   res.redirect("/login");
-  //   // } else {
-  //   res.render("signup");
-  //   // }
-  // });
 
   //LOGIN HTML ROUTES
   app.get("/login", function (req, res) {
-    // if (req.user) {
     res.render("login");
-    // } else {
-    // res.redirect("signup");
-    // }
   });
 
   // INDEX HTML ROUTES
   app.get("/", function (req, res) {
     res.render("signup");
-    // if (req.user) {
-    //   console.log("i logged in");
-    //   res.render("login");
-    // } else {
-    //   console.log("I didn't");
-    //   res.redirect("/signup");
-    // }
   });
 
   //LOGOUT HTML ROUTES
   app.get("/logout", function (req, res) {
-    // res.render("login");
     req.logout();
     res.redirect("/login")
   });
 
   //HOMEPAGE HTML ROUTES
   app.get("/home", isAuthenticated, function (req, res) {
-    // const currentUser = req.session.passport.user;
-    // if (currentUser) {
-    //   res.render("home", currentUser);
-    // }
-    // res.redirect("login");
-    // console.log("i am in home");
-    // console.log(req.session);
-    // res.render("home");
     res.render("home");
   });
-
-  // app.get("/home", isAuthenticated, function (req, res) {
-  //   res.render("home");
-  // });
 
   //PROFILE HTML ROUTES
   app.get("/profile", isAuthenticated, function (req, res) {
@@ -66,7 +35,3 @@ module.exports = function (app) {
     res.render("profile", user);
   });
 };
-
-// app.get("/profile", isAuthenticated, function (req, res) {
-//   res.render("profile");
-// });
