c1f81a611b5fa05ab1ae1f6ee1babac859b029a1
Selma Elgabalawy <salmosha88@gmail.com>
2020-12-21 19:32:08 -0800

    updating handlebar injections and redirections

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,51 +1,55 @@
 var path = require("path");
-// var isAuthenticated = require("../config/middleware/isAuthenticated");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
   //SIGNUP HTML ROUTES
-  app.get("/signup", function (req, res) {
-    // if (req.user) {
-    //   res.redirect("/login");
-    // } else {
-    res.render("signup");
-    // }
-  });
+  // app.get("/signup", function (req, res) {
+  //   // if (req.user) {
+  //   //   res.redirect("/login");
+  //   // } else {
+  //   res.render("signup");
+  //   // }
+  // });
 
   //LOGIN HTML ROUTES
   app.get("/login", function (req, res) {
     // if (req.user) {
     res.render("login");
     // } else {
-    //   res.redirect("signup");
+    // res.redirect("signup");
     // }
   });
 
   // INDEX HTML ROUTES
   app.get("/", function (req, res) {
-    if (req.user) {
-      console.log("i logged in");
-      res.render("login");
-    } else {
-      console.log("I didn't");
-      res.redirect("/signup");
-    }
+    res.render("signup");
+    // if (req.user) {
+    //   console.log("i logged in");
+    //   res.render("login");
+    // } else {
+    //   console.log("I didn't");
+    //   res.redirect("/signup");
+    // }
   });
 
   //LOGOUT HTML ROUTES
   app.get("/logout", function (req, res) {
-    res.render("login");
+    // res.render("login");
+    req.logout();
+    res.redirect("/login")
   });
 
   //HOMEPAGE HTML ROUTES
-  app.get("/home", function (req, res) {
-    const currentUser = req.session.passport.user;
-    if (currentUser) {
-      res.render("home", currentUser);
-    }
-    res.redirect("login");
-    console.log("i am in home");
-    console.log(req.session);
+  app.get("/home", isAuthenticated, function (req, res) {
+    // const currentUser = req.session.passport.user;
+    // if (currentUser) {
+    //   res.render("home", currentUser);
+    // }
+    // res.redirect("login");
+    // console.log("i am in home");
+    // console.log(req.session);
     // res.render("home");
+    res.render("home");
   });
 
   // app.get("/home", isAuthenticated, function (req, res) {
@@ -53,8 +57,13 @@ module.exports = function (app) {
   // });
 
   //PROFILE HTML ROUTES
-  app.get("/profile", function (req, res) {
-    res.render("profile");
+  app.get("/profile", isAuthenticated, function (req, res) {
+    let user = {
+      fullName: req.user.fullName,
+      email: req.user.email,
+      age: req.user.age
+    };
+    res.render("profile", user);
   });
 };
 
